5b182ce2aec2deac48493317fb07778ee6de6fc7
jorler <jorler_@hotmail.com>
2021-03-12 11:53:01 -0500

    segundo commit

---
+++ base de datos/bd_prueba.sql
@@ -0,0 +1,83 @@
+-- MariaDB dump 10.17  Distrib 10.4.14-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: bd_prueba
+-- ------------------------------------------------------
+-- Server version	10.4.14-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ciudad`
+--
+
+DROP TABLE IF EXISTS `ciudad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ciudad` (
+  `ciudad_id` int(11) NOT NULL,
+  `ciudad_nombre` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`ciudad_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ciudad`
+--
+
+LOCK TABLES `ciudad` WRITE;
+/*!40000 ALTER TABLE `ciudad` DISABLE KEYS */;
+INSERT INTO `ciudad` VALUES (1,'CALI'),(2,'MEDELLIN'),(3,'BOGOTA');
+/*!40000 ALTER TABLE `ciudad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `cliente_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cliente_documento` varchar(25) NOT NULL,
+  `cliente_nombre` varchar(50) NOT NULL,
+  `cliente_apellido` varchar(50) DEFAULT NULL,
+  `cliente_email` varchar(120) NOT NULL,
+  `id_ciudad` int(11) NOT NULL,
+  `cliente_user` varchar(50) NOT NULL,
+  `cliente_password` varchar(120) NOT NULL,
+  PRIMARY KEY (`cliente_id`,`cliente_documento`),
+  KEY `id_ciudad` (`id_ciudad`),
+  CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_ciudad`) REFERENCES `ciudad` (`ciudad_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'11147855114','JUAN','PEREZ','email@.com',1,'user','698d51a19d8a121ce581499d7b701668');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-12 10:44:48
--- controlador/registroDatos.php
+++ controlador/registroDatos.php
@@ -8,7 +8,7 @@ class RegistroDatos extends Conexion
 
     public function InsertDatos($documento,$nombre,$apellido,$email,$ciudad,$user,$password){
 
-      $insert= "insert into cliente values(default,$documento,'$nombre','$apellido','$email',$ciudad,'$user',md5('$password'))";
+      $insert= "insert into cliente values(default,$documento,upper('$nombre'),upper('$apellido'),'$email',$ciudad,'$user',md5('$password'))";
 
       $result=$this->conexion->query($insert);
       if($result){
